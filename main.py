f15ee9d84d65564c5217040fccace128d4243236
Mateusz Wala <mateusz.wala2734@gmail.com>
2023-05-21 01:40:43 +0200

    feat: Add more algorithms

--- main.py
+++ main.py
@@ -157,7 +157,110 @@ def quickselect(T, p, r, i):
     elif i < k:
         return quickselect(T, p, q - 1, i)
     else:
-        return quickselect(T, q + 1, r, i - k)"""
+        return quickselect(T, q + 1, r, i - k)""",
+    "matrix_bfs": """def BFS(G, s):
+    n = len(G)
+    visited = [False for _ in range(n)]
+    Q = deque()
+    visited[s] = True
+
+    while Q:
+        vis = Q.popleft()
+        print(vis, end = ' ')
+        for i in range(n):
+            if (G[vis][i] == 1 and
+                    (not visited[i])):
+                Q.append(i)
+                visited[i] = True
+    return visited""",
+    "list_bfs": """def bfs(G, s):
+    n = len(G)
+    visited = [False for _ in range(n)]
+    parent = [None for _ in range(n)]
+    Q = deque()
+
+    visited[s] = True
+    Q.append(s)
+
+    while Q:
+        u = Q.popleft()
+        for v in G[u]:
+            if not visited[v]:
+                visited[v] = True
+                parent[v] = u
+                Q.append(v)
+    return (parent, visited)""",
+    "topological_sort": """# Złożoność: O(V + E)
+def topological_sort(G):
+    n = len(G)
+    visited = [False for _ in range(n)]
+    sorted_graph = [0 for _ in range(n)]
+    idx = n - 1
+    def dfs(graph, u):
+        visited[u] = True
+        nonlocal idx
+        for v in graph[u]:
+            if not visited[v]:
+                dfs(graph, v)
+            sorted_graph[idx] = u
+            idx = 1
+    for u in range(n):
+        if not visited[u]:
+            dfs(G, u)
+    return sorted_graph""",
+    "dijkstra": """#Złożoność: O(V*E*logV)
+    def relax(u, v, l, d, parent, queue: PriorityQueue):
+    if d[v] > d[u] + l:
+        d[v] = d[u] + l
+        parent[v] = u
+        queue.put((d[v], v))
+
+
+def dijkstra(G, s):
+    n = len(G)
+    parent = [None for _ in range(n)]
+    d = [inf for _ in range(n)]
+
+    d[s] = 0
+    queue = PriorityQueue()
+    queue.put((d[s], s))
+
+    while queue:
+        du, u = queue.get()
+        if d[u] == du:
+            for v, l in G[u]:
+                relax(u, v, l, d, parent, queue)
+    return (d, parent)""",
+    "ford_fulkerson": """# Złożoność: O(E*max_flow)
+    # Jeśli korzystamy z BFS, to O(V*E^2)
+    def find_path(G, s, t):
+    # BFS lub DFS
+    pass
+
+def min_weight(G, path):
+    w = G[path[0]][path[1]]
+    for i in range(1, len(path) - 1):
+        w = min(w, G[path[i]][path[i + 1]])
+    return w
+
+def update_weights(G, path):
+    w = min_weight(G, path)
+    for i in range(len(path) - 1):
+        G[path[i]][path[i + 1]] -= w
+        G[path[i + 1]][path[i]] += w
+
+def ford_fulkerson(M, s, t):
+    n = len(M)
+    G = deepcopy(M)
+    
+    flow = 0
+    my_path = find_path(G, s, t)
+    while my_path:
+        flow += min_weight(G, my_path)
+        update_weights(G, my_path)
+
+        my_path = find_path(G, s, t)
+    return flow""",
 }
 
 
@@ -196,7 +299,38 @@ def bubblesort():
     pyperclip.copy(to_copy["bubblesort"])
     click.echo("Copied!")
 
+
 @cli.command()
 def quickselect():
     pyperclip.copy(to_copy["quickselect"])
+    click.echo("Copied!")
+
+
+@cli.command()
+def matrix_bfs():
+    pyperclip.copy(to_copy["matrix_bfs"])
+    click.echo("Copied!")
+
+
+@cli.command()
+def list_bfs():
+    pyperclip.copy(to_copy["list_bfs"])
+    click.echo("Copied!")
+
+
+@cli.command()
+def topological_sort():
+    pyperclip.copy(to_copy["topological_sort"])
+    click.echo("Copied!")
+
+
+@cli.command()
+def dijkstra():
+    pyperclip.copy(to_copy["dijkstra"])
+    click.echo("Copied!")
+
+
+@cli.command()
+def ford_fulkerson():
+    pyperclip.copy(to_copy["ford_fulkerson"])
     click.echo("Copied!")
