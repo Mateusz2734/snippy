f55513ab3c5715da0b48bb48a4a456c62c1b0b33
Mateusz Wala <mateusz.wala2734@gmail.com>
2024-02-22 22:29:58 +0100

    fix: Add NewApp function for easier testing

---
+++ app.go
@@ -0,0 +1,22 @@
+package main
+
+import "github.com/urfave/cli/v2"
+
+func NewApp(state *State) *cli.App {
+	return &cli.App{
+		Name:            "snippy",
+		Usage:           "CLI snippet manager",
+		HideHelpCommand: true,
+		Before: func(cCtx *cli.Context) error {
+			state.Snippets = ReadSnippets()
+			return nil
+		},
+		Action: DefaultAction(state),
+		Commands: []*cli.Command{
+			AddCommand(state),
+			GetCommand(state),
+			ListCommand(state),
+			DeleteCommand(state),
+		},
+	}
+}
--- main.go
+++ main.go
@@ -3,27 +3,12 @@ package main
 import (
 	"log"
 	"os"
-
-	"github.com/urfave/cli/v2"
 )
 
 func main() {
 	state := &State{}
 
-	app := &cli.App{
-		Before: func(cCtx *cli.Context) error {
-			state.Snippets = ReadSnippets()
-			return nil
-		},
-		Action: DefaultAction(state),
-		Commands: []*cli.Command{
-			AddCommand(state),
-			ListCommand(state),
-			EditCommand(state),
-			GetCommand(state),
-			DeleteCommand(state),
-		},
-	}
+	app := NewApp(state)
 
 	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
