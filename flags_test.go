ae438de225d27cc730f8381d31a9691331e4ada0
Mateusz Wala <mateusz.wala2734@gmail.com>
2024-02-29 16:09:53 +0100

    test: Implement tests for flags with validation or Env variable use

---
+++ flags_test.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/urfave/cli/v2"
+)
+
+func TestWithClipboard(t *testing.T) {
+	state := &State{}
+	buf := new(bytes.Buffer)
+	errBuf := new(bytes.Buffer)
+
+	app := cli.App{
+		Flags:     []cli.Flag{WithClipboard(state)},
+		Writer:    buf,
+		ErrWriter: errBuf,
+	}
+
+	t.Setenv("SNIPPY_USE_CLIPBOARD", "")
+
+	app.Run([]string{"app"})
+	assert.False(t, state.UseClipboard, "default clipboard value should be false")
+
+	app.Run([]string{"app", "--clipboard"})
+	assert.True(t, state.UseClipboard, "clipboard value should be true")
+
+	app.Run([]string{"app", "-c"})
+	assert.True(t, state.UseClipboard, "clipboard value should be true")
+
+	t.Setenv("SNIPPY_USE_CLIPBOARD", "true")
+
+	app.Run([]string{"app"})
+	assert.True(t, state.UseClipboard, "clipboard value should be true")
+
+	t.Setenv("SNIPPY_USE_CLIPBOARD", "false")
+
+	app.Run([]string{"app"})
+	assert.False(t, state.UseClipboard, "clipboard value should be false")
+
+	t.Setenv("SNIPPY_USE_CLIPBOARD", "invalid")
+
+	err := app.Run([]string{"app"})
+	assert.NotNil(t, err, "error expected")
+
+	t.Setenv("SNIPPY_USE_CLIPBOARD", "false")
+
+	err = app.Run([]string{"app", "--clipboard"})
+	assert.True(t, state.UseClipboard, "clipboard value should be true")
+}
+
+func TestWithPage(t *testing.T) {
+	state := &State{}
+	buf := new(bytes.Buffer)
+	errBuf := new(bytes.Buffer)
+
+	app := cli.App{
+		Flags:     []cli.Flag{WithPage(state)},
+		Writer:    buf,
+		ErrWriter: errBuf,
+	}
+
+	err := app.Run([]string{"app"})
+	assert.Equal(t, 1, state.CurrentPage, "default page should be 1")
+	assert.Nil(t, err, "no error expected")
+
+	err = app.Run([]string{"app", "--page", "2"})
+	assert.Equal(t, 2, state.CurrentPage, "page should be 2")
+	assert.Nil(t, err, "no error expected")
+
+	err = app.Run([]string{"app", "-p", "3"})
+	assert.Equal(t, 3, state.CurrentPage, "page should be 3")
+	assert.Nil(t, err, "no error expected")
+
+	err = app.Run([]string{"app", "-p", "a"})
+	assert.NotNil(t, err, "error expected")
+
+	err = app.Run([]string{"app", "-p", "0"})
+	assert.NotNil(t, err, "error expected")
+}
+
+func TestWithPageSize(t *testing.T) {
+	state := &State{}
+	buf := new(bytes.Buffer)
+	errBuf := new(bytes.Buffer)
+
+	app := cli.App{
+		Flags:     []cli.Flag{WithPageSize(state)},
+		Writer:    buf,
+		ErrWriter: errBuf,
+	}
+
+	t.Setenv("SNIPPY_PAGE_SIZE", "")
+	app.Run([]string{"app"})
+
+	assert.Equal(t, 20, state.PageSize, "default page size should be 20")
+	assert.Empty(t, errBuf.String(), "no error expected")
+
+	errBuf.Reset()
+	buf.Reset()
+
+	t.Setenv("SNIPPY_PAGE_SIZE", "30")
+	app.Run([]string{"app"})
+
+	assert.Equal(t, 30, state.PageSize, "page size should be 30")
+	assert.Empty(t, errBuf.String(), "no error expected")
+}
--- go.mod
+++ go.mod
@@ -3,19 +3,23 @@ module github.com/Mateusz2734/snippy
 go 1.22.0
 
 require (
+	github.com/alecthomas/chroma v0.10.0
 	github.com/andrew-d/go-termutil v0.0.0-20150726205930-009166a695a2
+	github.com/stretchr/testify v1.8.4
 	github.com/urfave/cli/v2 v2.27.1
 	golang.design/x/clipboard v0.7.0
 )
 
 require (
-	github.com/alecthomas/chroma v0.10.0 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dlclark/regexp2 v1.4.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 	golang.org/x/exp v0.0.0-20190731235908-ec7cb31e5a56 // indirect
 	golang.org/x/image v0.6.0 // indirect
 	golang.org/x/mobile v0.0.0-20230301163155-e0f57694e12c // indirect
 	golang.org/x/sys v0.7.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
